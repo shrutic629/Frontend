2ea3f703a278c97c46332780a8cce4055bbe1c27
Shruti Chindarkar <shrutic629@gmail.com>
2024-05-02 12:45:06 +0530

    updated

---
+++ React/Clothing_App/src/About_six.jsx
@@ -0,0 +1,6 @@
+function About_six()
+{
+    return(
+        
+    )
+}
--- React/Clothing_App/src/Header.jsx
+++ React/Clothing_App/src/Header.jsx
@@ -6,13 +6,13 @@ function Header()
         <div className="head_L">SIXTEEN <span>CLOTHING</span></div>
         <div className="head_R">
             <div className="R_innerbox">
-            <ul>
-            <li>Home</li>
-            <li>Our Products</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            </ul>
-        </div>
+                <ul>
+                <li>Home</li>
+                <li>Our Products</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                </ul>
+            </div>
         </div>
         </div>
     )
